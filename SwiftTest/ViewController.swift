27ad671dd9d62f3a80e16c7232439886857ebc94
minjongkim <rlaalswhd200@naver.com>
2022-04-03 16:15:52 +0900

    testing Gifu library

--- SwiftTest/ViewController.swift
+++ SwiftTest/ViewController.swift
@@ -2,18 +2,42 @@
 //  ViewController.swift
 //  SwiftTest
 //
-//  Created by 김민종 on 2022/03/10.
+//  Created by 김민종 on 2022/04/02.
 //
 
 import UIKit
+import Gifu
+import TinyConstraints
 
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var loadingGif: GIFImageView!
+    
+    @IBOutlet weak var image: UIImageView!
+    let gifImageView = GIFImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        animate()
     }
+    
+    
+    func animate() {
+//        self.loadingGif.animate(withGIFNamed: "loading")
+        
+        view.addSubview(gifImageView)
+        
+        gifImageView.animate(withGIFNamed: "loading")
+        
+//        gifImageView.centerInSuperview()
+        gifImageView.width(view.frame.width)
+        gifImageView.bottomToSuperview()
+
+    }
+    
+
 
 
 }
-
